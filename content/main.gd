7b1b92e689775263aba34172862116e8cdbb0547
BrainWave <pikadon92+1@gmail.com>
2025-05-18 23:06:47 +0800

    Executed Move (Pre-Effects)

--- content/main.gd
+++ content/main.gd
@@ -7,7 +7,14 @@ extends Node
 @export var timeline_used_move: DialogicTimeline
 
 func _ready() -> void:
+	Dialogic.text_signal.connect(_dialogic_text_signal)
 	Dialogic.start_timeline(timeline_commands)
+
+
+func _dialogic_text_signal(event: String) -> void:
+	match event:
+		"execute_move":
+			print("Tackle")
 
 
 func _on_move_pressed() -> void:
---
+++ content/unit.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@export var unit_bar: PanelContainer
+
+var max_hp := 10
+var cur_hp := max_hp
