ea3bbd9b517c45a9f0cd0f0d40ce1302571920ec
BrainWave <pikadon92+1@gmail.com>
2025-05-18 16:57:37 +0800

    "Someone used Tackle!"

--- content/main.gd
+++ content/main.gd
@@ -3,5 +3,12 @@ extends Node
 ## Timeline for asking a general command.
 @export var timeline_commands: DialogicTimeline
 
+## Timeline declaring unit using a move.
+@export var timeline_used_move: DialogicTimeline
+
 func _ready() -> void:
 	Dialogic.start_timeline(timeline_commands)
+
+
+func _on_move_pressed() -> void:
+	Dialogic.start_timeline(timeline_used_move)
