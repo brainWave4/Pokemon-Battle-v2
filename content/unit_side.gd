671cffc5dd8bd55bd4b90b7496266becf4d919b7
BrainWave <pikadon92+1@gmail.com>
2025-05-25 09:59:35 +0800

    Added Check on Team for Units Able to Fight

--- content/unit_side.gd
+++ content/unit_side.gd
@@ -3,3 +3,12 @@ class_name UnitSide
 
 ## A group of units taking part of this battle.
 @export var team: Array[BattleUnit]
+
+## Checks whether there are any remaining units
+## that are able to battle.
+func has_available_units() -> bool:
+	for bu: BattleUnit in team:
+		if bu.cur_hp:
+			return true
+	
+	return false
